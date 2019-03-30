8db09b50cadd30b038ef0434bd29c8e9bf47ae35
Carlos Gallego <gallegohh@outlook.com>
2019-03-30 13:04:44 +0100

    Añadido el controlador de Empleados y cargados los modelos de northwind en el scaffolding del proyecto. Creados los métodos Index y Create del controlador Employees además de sus vistas.

---
+++ TrainingForMVC/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrainingForMVC.Models;
+
+namespace TrainingForMVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private ModelNorthwind db;
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Employees());
+        }
+
+        public EmployeesController()
+        {
+            db = new ModelNorthwind();
+        }
+    }
+}
